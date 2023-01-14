dd6fdde3d266efd55e5f22226061d3b783a2fa1b
kkkovalchukkk <student.kovalchuk@yandex.ru>
2023-01-15 02:38:41 +0700

    finished main page 1920

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,13 +1,25 @@
 var scrollPos = 0;
+
+$(".arrow-up").click(() => $("html,body").animate({ scrollTop: 0 }, 500));
+
+if (window.scrollY >= 78) {
+  document.querySelector(".bottom-header").classList.add("fixed-header");
+}
 $(window).scroll(function () {
   var st = $(this).scrollTop();
   if (st > scrollPos) {
     if (scrollPos >= 78) {
       document.querySelector(".bottom-header").classList.add("fixed-header");
     }
+    if (scrollPos >= 250) {
+      document.querySelector(".arrow-up").classList.remove("hidden");
+    }
   } else {
     if (scrollPos <= 78) {
       document.querySelector(".bottom-header").classList.remove("fixed-header");
+    }
+    if (scrollPos <= 250) {
+      document.querySelector(".arrow-up").classList.add("hidden");
     }
   }
   scrollPos = st;
